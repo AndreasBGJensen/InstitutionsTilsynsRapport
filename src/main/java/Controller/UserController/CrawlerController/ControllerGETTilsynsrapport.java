fcbf31c9a0215d150560d60fd93dabec8465aca9
AndreasBGJensen <bgandreas@hotmail.com>
2019-11-24 14:46:19 +0100

    Crawler controller

---
+++ src/main/java/Controller/UserController/CrawlerController/ControllerGETTilsynsrapport.java
@@ -0,0 +1,37 @@
+package Controller.UserController.CrawlerController;
+
+
+
+import Model.Crawler.TraverserURL;
+import Model.Crawler.documentURL;
+import Model.Crawler.soegningURL;
+import Model.Downloader.DownloadPDF;
+import Model.ExtractTextFromPdf.PDFManager;
+import Model.GetBaseUrl.IBaseURL;
+import Model.Vuggestue;
+
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+public class ControllerGETTilsynsrapport {
+    documentURL doc = new documentURL();
+    IBaseURL getbaseUrl = new soegningURL(doc);
+    TraverserURL traversering;
+    List<Vuggestue> vuggestuer;
+    PDFManager extractPdf;
+
+    public ControllerGETTilsynsrapport(){}
+
+    public Response getTilsynsrapport(String searchAddress, String keyword){
+
+        vuggestuer = getbaseUrl.getVuggestuer(searchAddress,keyword);
+        traversering = new TraverserURL(doc,vuggestuer);    //Adding the links to the vuggestue.
+        extractPdf = new PDFManager(vuggestuer);
+        return Response.ok().entity(vuggestuer).build();
+    }
+
+
+
+
+}
+

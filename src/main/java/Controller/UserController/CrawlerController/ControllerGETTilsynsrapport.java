2377ac36ba59844db161484109542fea31cb459a
AndreasBGJensen <bgandreas@hotmail.com>
2019-11-24 14:49:41 +0100

    Model Crawler: - Traversing - Get base informations - Connections to Jsoup

--- src/main/java/Controller/UserController/CrawlerController/ControllerGETTilsynsrapport.java
+++ src/main/java/Controller/UserController/CrawlerController/ControllerGETTilsynsrapport.java
@@ -2,9 +2,6 @@ package Controller.UserController.CrawlerController;
 
 
 
-import Model.Crawler.TraverserURL;
-import Model.Crawler.documentURL;
-import Model.Crawler.soegningURL;
 import Model.Downloader.DownloadPDF;
 import Model.ExtractTextFromPdf.PDFManager;
 import Model.GetBaseUrl.IBaseURL;

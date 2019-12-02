cf6a68dd9f38579660c86d2ffacce3500765addf
AndreasBGJensen <bgandreas@hotmail.com>
2019-12-02 06:50:16 +0100

    Add comments to MongoConnection

--- src/main/java/Model/Database/MongoConnector.java
+++ src/main/java/Model/Database/MongoConnector.java
@@ -3,13 +3,24 @@ import com.mongodb.DB;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
+
+/*
+Returns the connection to MongoDB. This class is a Singleton.
+ */
+
 public class MongoConnector {
+
+
     static MongoConnector instance;
     private final static String database = "aa";
     private MongoClient mongoClient;
+
+
     private MongoConnector(){
         this.mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://User:Abekat15@cluster0-hbffi.mongodb.net/test?retryWrites=true&w=majority"));
     }
+
+
     public static DB getInstance(){
         if(instance == null){
             instance = new MongoConnector();

67660e685d506ea035d656444c238e1d51787d47
claeslindhardt1 <claeslindhardt@gmail.com>
2019-12-03 11:59:49 +0100

    Started storing the token at the client, now trying to implement the authfilter

---
+++ src/main/webapp/precache-manifest.32f0ed2fce2c9eb8a39bc361dd5d4d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122db714969bf87ddf5859e78a2304d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ca223d3fcf8be633c7a",
+    "url": "/static/css/2.9a008ecd.chunk.css"
+  },
+  {
+    "revision": "ecb13dfe5f8a0c51e67d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ca223d3fcf8be633c7a",
+    "url": "/static/js/2.38b799b7.chunk.js"
+  },
+  {
+    "revision": "ecb13dfe5f8a0c51e67d",
+    "url": "/static/js/main.a09cdcb4.chunk.js"
+  },
+  {
+    "revision": "64ab56e38a1c971639bb",
+    "url": "/static/js/runtime-main.2302c2fb.js"
+  }
+]);
---
+++ src/main/webapp/precache-manifest.43b2fc682b42e858bb81e13b64fb5663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80575a87dd75fac254d52a97fee9f72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ca223d3fcf8be633c7a",
+    "url": "/static/css/2.9a008ecd.chunk.css"
+  },
+  {
+    "revision": "68af69eab7d85f6576e2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8ca223d3fcf8be633c7a",
+    "url": "/static/js/2.38b799b7.chunk.js"
+  },
+  {
+    "revision": "68af69eab7d85f6576e2",
+    "url": "/static/js/main.27526fba.chunk.js"
+  },
+  {
+    "revision": "64ab56e38a1c971639bb",
+    "url": "/static/js/runtime-main.2302c2fb.js"
+  }
+]);

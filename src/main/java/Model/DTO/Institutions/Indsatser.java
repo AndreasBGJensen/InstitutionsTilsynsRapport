91cee3cb1d7bf1ff6cc463c8c310eea71c03c30a
Troels Just Christoffersen <tjoels@gmail.com>
2019-12-05 17:33:56 +0100

    add lombok getter and setter

---
+++ src/main/java/Model/DTO/Institutions/Indsatser.java
@@ -0,0 +1,46 @@
+package Model.DTO.Institutions;
+
+import lombok.*;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Indsatser {
+
+    private HashMap<String, String> indsatser = new HashMap<String, String>();
+    private String tilsyn = "";
+
+    public Indsatser(JSONObject json) {
+        this.setIndsatser((HashMap) json.get("indstsers"));
+        this.setTilsyn((String) json.get("tilsyn"));
+
+    }
+
+    public Indsatser() {
+
+    }
+
+    public void addIndsats(String beskrivelse, String indsats) {
+        indsatser.put(beskrivelse, indsats);
+    }
+
+    public void setTilsyn(String tilsyn) {
+        this.tilsyn = tilsyn;
+    }
+
+    public HashMap<String, String> getIndsatser() {
+        return indsatser;
+    }
+
+    public void setIndsatser(HashMap<String, String> indsatser) {
+        this.indsatser = indsatser;
+    }
+
+
+}

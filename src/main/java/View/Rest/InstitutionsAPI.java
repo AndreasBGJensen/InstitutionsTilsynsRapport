1d1d01d33b72328ac193f57cea6ec279cf927767
Troels Just Christoffersen <tjoels@gmail.com>
2019-12-05 14:10:08 +0100

    do not return Response, implement getters and setters for SearchParam

--- src/main/java/View/Rest/InstitutionsAPI.java
+++ src/main/java/View/Rest/InstitutionsAPI.java
@@ -1,17 +1,13 @@
 package View.Rest;
+
 import Controller.Controller;
 import Model.DTO.Institutions.Vuggestue;
 import Model.DTO.SearchParam;
-
+import View.Rest.Exceptions.DbException;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-
 import java.util.List;
-
-import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 
 
 @Path("institution")
@@ -21,34 +17,32 @@ public class InstitutionsAPI {
 
     Controller controller = new Controller();
 
-
     //TODO: Changing Response to some other datatype
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response postLoginData(SearchParam search){
+    public List<Vuggestue> postLoginData(SearchParam search) throws DbException {   //TODO: right way to to this by throwing exception here?
         System.out.println(search.toString());
-        try {
-            return Response.ok().entity(controller.getUserSearchInstitutionsFromDatabase(search.getVejnavn(), search.getPostNr())).build();
-        }catch (Exception e) {
-            return Response.status(INTERNAL_SERVER_ERROR).entity("Somthing happen").build();
-        }
-    }
+        return controller.getUserSearchInstitutionsFromDatabase(search.getVejnavn(), search.getPostNr());
 
+//        try {
+//            return controller.getUserSearchInstitutionsFromDatabase(search.getVejnavn(), search.getPostNr());
+//        } catch (Exception e) {
+//            return Response.status(INTERNAL_SERVER_ERROR).entity("Somthing happen").build();
+//        }
+    }
 
     @Path("update")
     @POST
-    public String deepUpdateDatabase(SearchParam search){
+    public String deepUpdateDatabase(SearchParam search) {
         System.out.println(search.toString());
-        return controller.updateInstitutionQuery(search.getVejnavn(),search.getPostNr());
-
+        return controller.updateInstitutionQuery(search.getVejnavn(), search.getPostNr());
     }
 
-   @Path("/all")
+    @Path("/all")
     @GET
     public Iterable<Vuggestue> getAllVuggestuer() {
 
-return controller.getAllInstitutions();
-   }
-
+        return controller.getAllInstitutions();
+    }
 }

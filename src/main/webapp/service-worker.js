48be1c8cf8bce69471406685d4f7ac93b8ef1c4a
AndreasBGJensen <bgandreas@hotmail.com>
2019-12-06 06:02:50 +0100

    Delete old build files add new files

--- src/main/webapp/service-worker.js
+++ src/main/webapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24b3d92359c372541ecc07d56b7fc642.js"
+  "/precache-manifest.f67e2ef1d3148c5f7311ac18f996824f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

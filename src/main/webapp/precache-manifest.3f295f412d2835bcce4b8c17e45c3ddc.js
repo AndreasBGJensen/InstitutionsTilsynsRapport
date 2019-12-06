07b72e926b2d69376dadbb7ad5c0123ef839e458
AndreasBGJensen <bgandreas@hotmail.com>
2019-12-06 01:47:38 +0100

    mavenBuild change base URL to 'production'

---
+++ src/main/webapp/precache-manifest.3f295f412d2835bcce4b8c17e45c3ddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19003f767cfb706a3a6e0051e53055d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad4b2cb06368274bb00d",
+    "url": "/static/css/2.9a008ecd.chunk.css"
+  },
+  {
+    "revision": "2830d3140209fbbcdc39",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ad4b2cb06368274bb00d",
+    "url": "/static/js/2.92c2497f.chunk.js"
+  },
+  {
+    "revision": "2830d3140209fbbcdc39",
+    "url": "/static/js/main.34f84e8c.chunk.js"
+  },
+  {
+    "revision": "64ab56e38a1c971639bb",
+    "url": "/static/js/runtime-main.2302c2fb.js"
+  }
+]);

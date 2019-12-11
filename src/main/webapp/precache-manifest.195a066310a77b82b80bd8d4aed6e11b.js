588f918e8405568a206be171f3645b2e970a4bc6
Troels Just Christoffersen <tjoels@gmail.com>
2019-12-11 16:07:25 +0100

    change database to DB "tilsyn" and maven package, add additional build output

---
+++ src/main/webapp/precache-manifest.195a066310a77b82b80bd8d4aed6e11b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c8d853ebd20f7a4e22766f15bc9b21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40476083e87da2676c21",
+    "url": "/static/css/2.7c4efcaa.chunk.css"
+  },
+  {
+    "revision": "a0f3352ae9cd6e445f17",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "40476083e87da2676c21",
+    "url": "/static/js/2.fb6b6abb.chunk.js"
+  },
+  {
+    "revision": "a0f3352ae9cd6e445f17",
+    "url": "/static/js/main.b58ec44e.chunk.js"
+  },
+  {
+    "revision": "64ab56e38a1c971639bb",
+    "url": "/static/js/runtime-main.2302c2fb.js"
+  }
+]);
